54c18c950d9d1b4374b1281f63e47f36917e3986
amabdalla <amabdalla@E815WL230006.org.cbe.ab.ca>
2025-02-26 13:12:30 -0700

    1:12PM

--- Chapter11/src/SkillBuilders/StatsP2.java
+++ Chapter11/src/SkillBuilders/StatsP2.java
@@ -4,118 +4,120 @@ import java.io.*;
 import java.text.NumberFormat;
 import java.util.Scanner;
 
+
 public class StatsP2 
 {
 
 	public static void main(String[] args) 
 	{
-	
 		File dataFile;
 		FileReader in;
-		String filename;
 		BufferedReader readFile;
 		FileWriter out;
 		BufferedWriter writeFile;
-		int numStu;
-		String StuName, score;
+		
+		Scanner input = new Scanner(System.in);
+		
+		String fileName;
+		int numStud;
+		
+		String stuName, score;
+		
 		double scoreValue;
 		double lowScore = 100;
 		double highScore = 0;
 		double avgScore;
-		double totalScores = 0;
+		double totalScore = 0;
 		int numScores = 0;
 		
-		NumberFormat percent = NumberFormat.getPercentInstance();
-		Scanner input = new Scanner(System.in);
 		
-		//Write data to file
-		System.out.println("Enter file name for "+
-						"storing names and grades: ");
-		filename = input.next();
+		System.out.println("Enter file name: ");
+		fileName = input.next();
 		
 		System.out.println("How many students? ");
-		numStu = input.nextInt();
-
+		numStud = input.nextInt();
+		
+		//Write data to file
 		try
 		{
-			dataFile = new File(filename);
+			dataFile = new File(fileName);
 			out = new FileWriter(dataFile);
 			writeFile = new BufferedWriter(out);
 			
-			for(int i=0; i < numStu; i++) 
+			for(int i=0; i < numStud; i++)
 			{
 				System.out.println("Enter student name: ");
-				StuName = input.next();
+				stuName = input.next();
 				
-				System.out.println("Enter test score: ");
+				System.out.println("Enter test scores: ");
 				score = input.next();
 				
-				writeFile.write(StuName);
+				writeFile.write(stuName);
 				writeFile.newLine();
 				writeFile.write(score);
 				writeFile.newLine();
 			}
+			
 			writeFile.close();
 			out.close();
-			System.out.println("Data written to file." );
+			System.out.println("Data written to file");
+			
 		}
 		catch (IOException e) 
-        {
-			System.out.println("Problem reading file.");
-			System.err.println("IOException: " + e.getMessage());
-        }
+		{   
+			System.out.println("Problem writing to file.");   System.err.println("IOException: " + e.getMessage());  
+		}
 		
-		//Read data from file and process
+		
+		//Read data from the file
 		try
 		{
-
-			dataFile = new File(filename);
+			dataFile = new File(fileName);
 			in = new FileReader(dataFile);
 			readFile = new BufferedReader(in);
 			
-			while((StuName = readFile.readLine()) != null)
+			while((stuName = readFile.readLine()) != null)
 			{
 				score = readFile.readLine();
-				System.out.println(StuName + " "+score);
-				numScores = numScores + 1;
+				System.out.println(stuName + " "+ score);
+				
+				numScores += 1;
+				
 				scoreValue = Double.parseDouble(score);
-				totalScores = totalScores + scoreValue;
+				totalScore += scoreValue;
 				
 				if(scoreValue < lowScore)
-				{
 					lowScore = scoreValue;
-				}
-				if(scoreValue > lowScore)
-				{
+				
+				if(scoreValue > highScore)
 					highScore = scoreValue;
-				}
 				
 			}
-			avgScore = totalScores / numScores;
-			System.out.println("Low Score = " +
-								percent.format(lowScore/100));
-			System.out.println("High Score = " +
-					percent.format(highScore/100));
-			System.out.println("Average = " +
-					percent.format(avgScore/100));
+			avgScore = totalScore / numScores;
 			
-			in.close();
+			NumberFormat percent = NumberFormat.getPercentInstance();
+
 			
-		
+			System.out.println("Low score = "
+							+ percent.format(lowScore/100));
 			
+			System.out.println("High score = "
+					+ percent.format(highScore/100));
+			
+			System.out.println("Average =" 
+					+ percent.format(avgScore/100));
+			
+			readFile.close();
+			in.close();			
 		}
 		catch (FileNotFoundException e) 
-        {
-			System.out.println("File does not exist or could not be found.");
-			System.err.println("FileNotFoundException: " + e.getMessage());
-
-        } 
+		{    
+			System.out.println("File could not be found.");      System.err.println("FileNotFoundException: "             + e.getMessage());
+		}
 		catch (IOException e) 
-        {
-			System.out.println("Problem reading file.");
-			System.err.println("IOException: " + e.getMessage());
-        }
-		
+		{   
+			System.out.println("Problem writing to file.");   System.err.println("IOException: " + e.getMessage());  
+		}
 		
 	}
 
