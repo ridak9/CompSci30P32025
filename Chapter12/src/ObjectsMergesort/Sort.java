471f05cfa3f959353098b7f08129b793ff5a45fa
amabdalla <amabdalla@E815WL230006.org.cbe.ab.ca>
2025-04-28 13:07:20 -0600

    1:07pm

---
+++ Chapter12/src/ObjectsMergesort/Sort.java
@@ -0,0 +1,49 @@
+package ObjectsMergesort;
+
+public class Sort 
+{
+	
+	private static void merge(Comparable[] items, int start, int mid, int end)
+	{
+		Comparable[] temp = new Comparable[items.length];
+		
+		int pos1 = start;
+		int pos2 = mid + 1;
+		int spot = start;
+		
+		while(!(pos1 > mid && pos2 > end))
+		{
+			if((pos1 > mid) || ((pos2 <= end) && (items[pos2].compareTo(items[pos1]) < 0)))
+			{
+				temp[spot] = items[pos2];
+				pos2 += 1;
+			}
+			else
+			{
+				temp[spot] = items[pos1];
+				pos1 += 1;
+			}
+			spot += 1;
+		}
+		
+		//Copy values from temp back to item
+		for(int i = start; i <= end; i++)
+		{
+			items[i] = temp[i];
+		}
+	}
+	
+	//Sorts items[start....end] is sorted low to high
+	
+	public static void mergesort(Comparable[] items, int start, int end)
+	{
+		if(start < end)
+		{
+			int mid = (start + end) / 2;
+			mergesort(items, start, mid);
+			mergesort(items, mid + 1, end);
+			merge(items, start, mid, end);
+		}
+	}
+
+}
